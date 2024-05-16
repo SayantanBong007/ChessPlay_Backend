63307ef7c40747a36b9606680a2e5c3b282af058
Sayantan Gain <sayantangain63@gmail.com>
2024-05-16 12:00:45 +0530

    update server.js

--- server.js
+++ server.js
@@ -1,26 +1,20 @@
-const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const appServer = require("./app");
 dotenv.config();
-
-const app = express();
 
 // Connect to MongoDB database
 mongoose
   .connect(process.env.MONGODB_URL)
-  .then(() => {
+  .then((con) => {
     console.log("Connected to database successfully");
   })
   .catch((err) => {
     console.log("Error while connecting to DB", err);
   });
 
-app.get("/", (req, res) => {
-  res.send("Server is running correctly");
-});
-
 // Set the port for the server
 const port = process.env.PORT || 3001;
-app.listen(port, () => {
+appServer.listen(port, () => {
   console.log("Listening on port ", port);
 });
