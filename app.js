86926185f2b62f623e7af5e5d7b056a9902dfb38
Sayantan Gain <sayantangain63@gmail.com>
2024-05-18 21:53:50 +0530

    updated

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const appServer = http.createServer(app);
 // Create a socket.io server and configure CORS settings
 const io = socketIO(appServer, {
   cors: {
-    origin: "https://chess-play-seven.vercel.app/",
+    origin: "https://chess-play-seven.vercel.app",
     methods: ["GET", "POST"],
   },
 });
