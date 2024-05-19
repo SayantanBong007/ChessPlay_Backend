dd06900789242c6be774d4e28899ea56940edadd
Sayantan Gain <sayantangain63@gmail.com>
2024-05-19 22:30:22 +0530

    Cors updated

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "https://chess-play-seven.vercel.app",
+    origin: "*",
     methods: ["GET", "POST", "PATCH", "PUT", "DELETE"],
     credentials: true,
     allowEIO3: true,
@@ -25,13 +25,12 @@ const appServer = http.createServer(app);
 
 // Create a socket.io server and configure CORS settings
 const io = socketIO(appServer, {
-  // cors: {
-  //   origin: "https://chess-play-seven.vercel.app",
-  //   methods: ["GET", "POST", "PATCH", "PUT", "DELETE"],
-  //   credentials: true,
-  //   allowEIO3: true,
-  // },
-  transports: ["websocket", "polling", "flashsocket"],
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST", "PATCH", "PUT", "DELETE"],
+    credentials: true,
+    allowEIO3: true,
+  },
 });
 
 // Socket.io event listeners
